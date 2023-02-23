2cd5098e3f43939d2d9f75fc3e94a4d90a92e17a
Kevin on W <masonke@gmail.com>
2023-02-23 16:29:40 -0500

    Initial save

---
+++ start-sharkd.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+#
+#######################################
+## Make sure things run consistently ##
+#######################################
+export LANGUAGE=C
+export LANG=C
+export LC_MESSAGES=C
+export LC_TIME=C
+
+########################
+## Script starts here ##
+########################
+
+if pgrep [s]harkd
+then
+  echo "Sharkd is already running"
+else
+  echo "Deleting the old socket file"
+  if [[ -e /private/tmp/sharkd.sock ]] ; then
+	rm /private/tmp/sharkd.sock
+  fi
+  echo "Starting sharkd"
+  sharkd unix:/private/tmp/sharkd.sock
+  pgrep [s]harkd
+fi
+ 
