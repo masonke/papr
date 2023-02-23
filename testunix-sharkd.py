06c3eddc98cdaa2fc1c8ec612e743d1f407d7abc
Kevin on W <masonke@gmail.com>
2023-02-23 09:51:32 -0500

    Added more pcap tests and increased buffer to catch all data

--- testunix-sharkd.py
+++ testunix-sharkd.py
@@ -4,6 +4,7 @@ import os
 
 # https://wiki.wireshark.org/sharkd-JSON-RPC-Request-Syntax
 # https://stackoverflow.com/questions/20777173/add-variable-value-in-a-json-string-in-python
+#https://wiki.wireshark.org/sharkd-Info-Request-Output-Example
 
 
 uds_server_address = '/private/tmp/sharkd.sock'
@@ -14,15 +15,15 @@ pcap_name = '/private/tmp/small.pcapng'
 #   rm /tmp/sharkd.sock
 # fi
 
-
 def get_json_bytes(json_string):
     return bytes((json_string + '\n'), 'utf-8')
 
+# Increased receive size to 32K to capture all the payload.
+# If received data is cut off, increase this value.
 def json_send_recv(s, json) -> str:
     s.sendall(get_json_bytes(json))
-    data = s.recv(1024)
+    data = s.recv(32768)
     return data[:-4].decode('utf-8')
-
 
 s = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
 print('c: Connecting to ' + uds_server_address)
@@ -32,13 +33,34 @@ except socket.error as msg:
     print(msg)
     sys.exit(1)
 
+# Load the pcap file
 json_string = '{"jsonrpc":"2.0","id":1,"method":"load","params":{"file":"'+pcap_name+'"}}'
 print('s: ' + json_string)
 
 recv_json = json_send_recv(s, json_string)
 print('r: ' + recv_json)
 
+# Get the staus of the pcap
 json_string = '{"jsonrpc":"2.0","id":2,"method":"status"}'
+print('s: ' + json_string)
+
+rx_json = json_send_recv(s, json_string)
+print('r: ' + rx_json)
+
+json_string = '{"jsonrpc":"2.0","id":2,"method":"analyse"}'
+print('s: ' + json_string)
+
+rx_json = json_send_recv(s, json_string)
+print('r: ' + rx_json)
+
+# https://wiki.wireshark.org/sharkd-JSON-RPC-Request-Syntax#tap
+json_string = '{"jsonrpc":"2.0","id":2,"method":"tap","params":{"tap0":"expert"}}'
+print('s: ' + json_string)
+
+rx_json = json_send_recv(s, json_string)
+print('r: ' + rx_json)
+
+json_string = '{"jsonrpc":"2.0","id":2,"method":"tap","params":{"tap0":"seqa:tcp"}}'
 print('s: ' + json_string)
 
 rx_json = json_send_recv(s, json_string)
